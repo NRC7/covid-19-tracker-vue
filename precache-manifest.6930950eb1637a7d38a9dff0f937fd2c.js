e64e78247c9486dfad3fa3b7b479f89b1fddd19e
nrc7 <n.rodriguez.c7@gmail.com>
2021-08-23 01:31:14 -0400

    New Deployment

---
+++ precache-manifest.6930950eb1637a7d38a9dff0f937fd2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b23c4430f86265d25e1",
+    "url": "/covid-19-tracker-vue/css/app.4b42fa2b.css"
+  },
+  {
+    "revision": "549947b6ecfc963d5df7a46e04847390",
+    "url": "/covid-19-tracker-vue/img/hourglass.549947b6.gif"
+  },
+  {
+    "revision": "aab606bd9498ce9057bb9e1bf24952b2",
+    "url": "/covid-19-tracker-vue/index.html"
+  },
+  {
+    "revision": "4b23c4430f86265d25e1",
+    "url": "/covid-19-tracker-vue/js/app.3265eb1b.js"
+  },
+  {
+    "revision": "a865d0606baeb6175117",
+    "url": "/covid-19-tracker-vue/js/chunk-vendors.db315cfd.js"
+  },
+  {
+    "revision": "c87de59b152b0beb2a1e72de3198f5ba",
+    "url": "/covid-19-tracker-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covid-19-tracker-vue/robots.txt"
+  }
+]);
